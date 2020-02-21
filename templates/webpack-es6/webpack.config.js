ede13055a60dd9e3678154d953fb662e578c9c63
js-jslog <jhs4jbs@hotmail.co.uk>
2020-02-21 18:29:08 +0000

    Add webpack-dev-server to the webpack template

--- templates/webpack-es6/webpack.config.js
+++ templates/webpack-es6/webpack.config.js
@@ -4,9 +4,15 @@ const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 const ManifestPlugin = require('webpack-manifest-plugin');
 
 module.exports = {
+  mode: 'development',
   entry: {
     app: './src/index.js',
     print: './src/print.js',
+  },
+  devtool: 'inline-source-map',
+  devServer: {
+    contentBase: './dist',
+    port: 3000,
   },
   plugins: [
     new ManifestPlugin(),
