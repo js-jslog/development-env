2592546478cd8c023db6cfffccd268e060530c85
js-jslog <josephsinfield@yahoo.com>
2024-03-11 09:11:02 +0000

    Get rid of *all* executable bits from service file
    See previous commit.

--- setup-wsl-socat-docker-clip-service.sh
+++ setup-wsl-socat-docker-clip-service.sh
@@ -3,7 +3,7 @@
 sudo apt update && sudo apt install socat
 
 sudo cp ./socat-docker-clip.service /etc/systemd/system/socat-docker-clip.service
-sudo chmod u-x /etc/systemd/system/socat-docker-clip.service
+sudo chmod 644 /etc/systemd/system/socat-docker-clip.service
 
 sudo systemctl enable socat-docker-clip.service
 sudo systemctl start socat-docker-clip.service
