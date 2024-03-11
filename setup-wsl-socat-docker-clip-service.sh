380ec9389490f9717078fa4b1f19f752b6317d04
js-jslog <josephsinfield@yahoo.com>
2024-03-11 09:05:43 +0000

    Correct typo in systemctl command

--- setup-wsl-socat-docker-clip-service.sh
+++ setup-wsl-socat-docker-clip-service.sh
@@ -5,4 +5,4 @@ sudo apt update && sudo apt install socat
 sudo cp ./socat-docker-clip.service /etc/systemd/system/socat-docker-clip.service
 
 sudo systemctl enable socat-docker-clip.service
-sudo systemstl start socat-docker-clip.service
+sudo systemctl start socat-docker-clip.service
