d4c2ee0f9ebb3eb251693dda0cfbb79a057edba3
js-jslog <josephsinfield@yahoo.com>
2024-03-11 08:53:24 +0000

    Create service alternative to socat-listener-wsl.sh
    This will be preferable if it works. It'll mean that we only have to set
    up the listener once.

---
+++ setup-wsl-socat-docker-clip-service.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+sudo apt update && sudo apt install socat
+
+sudo cp ./socat-docker-clip.service /etc/systemd/system/socat-docker-clip.service
+
+sudo systemctl enable socat-docker-clip.service
+sudo systemstl start socat-docker-clip.service
